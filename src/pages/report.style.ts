639cadc41e886fbb9cb44d24f9050e39e7cbead9
Gabul99 <hclee99@kaist.ac.kr>
2023-11-07 23:49:52 +0900

    Report Screen Layout 잡음

---
+++ src/pages/report.style.ts
@@ -0,0 +1,62 @@
+import { styled } from 'styled-components';
+import { Colors } from '../styles/colors';
+
+export const Container = styled.div`
+  flex-grow: 1;
+  min-width: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  height: 104px;
+  padding: 40px;
+
+  font-size: 32px;
+  font-weight: 700;
+
+  flex-shrink: 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+`;
+
+export const Body = styled.body`
+  width: 100%;
+  flex-grow: 1;
+  flex-basis: 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const LeftListArea = styled.div`
+  width: 360px;
+  height: 100%;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  flex-shrink: 0;
+  overflow-y: auto;
+
+  .title {
+    font-size: 32px;
+    font-weight: 500;
+  }
+`;
+
+export const Contents = styled.div`
+  flex-grow: 1;
+  min-width: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+  gap: 16px;
+
+  background-color: ${Colors.Tertiary};
+`;
--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -1,5 +1,17 @@
+import * as S from './report.style';
+
 const Report: React.FC = () => {
-  return <div>Report</div>;
+  return (
+    <S.Container>
+      <S.Header>Reports</S.Header>
+      <S.Body>
+        <S.LeftListArea>
+          <h2 className="title">List</h2>
+        </S.LeftListArea>
+        <S.Contents>contents</S.Contents>
+      </S.Body>
+    </S.Container>
+  );
 };
 
 export default Report;
