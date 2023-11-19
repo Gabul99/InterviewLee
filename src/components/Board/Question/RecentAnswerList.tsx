1332708f3ed96535c8a8a6a52827151591f16cce
Gabul99 <hclee99@kaist.ac.kr>
2023-11-19 22:21:35 +0900

    Recent Answers에도 막음

--- src/components/Board/Question/RecentAnswerList.tsx
+++ src/components/Board/Question/RecentAnswerList.tsx
@@ -35,9 +35,14 @@ interface RecentAnswerProps {
 }
 
 const RecentAnswer: React.FC<RecentAnswerProps> = ({ id, answer }) => {
+  const { profile, setLoginModalOpen } = useAuthContext();
   const { setSelectedAnswerId, setSelectedQuestionId } = useQuestionContext();
 
   const handleClick = () => {
+    if (!profile) {
+      setLoginModalOpen(true);
+      return;
+    }
     setSelectedQuestionId(id);
     setSelectedAnswerId(answer.id);
   };
