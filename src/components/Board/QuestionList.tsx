1600f459c312a6d6368ee6ea15954697fdf3bbd2
jaehuiui <jaehuiui@woowahan.com>
2023-12-02 23:58:46 +0900

    fix: 태그 없는 경우 핸들링

--- src/components/Board/QuestionList.tsx
+++ src/components/Board/QuestionList.tsx
@@ -19,10 +19,10 @@ const QuestionList: React.FC = () => {
   const filteredQuestions = useMemo(() => {
     let result = questions;
     if (tagFilters.length > 0) {
-      result = result.filter((q) => q.tags.some((element) => tagFilters.includes(element)));
+      result = result.filter((q) => (q.tags ?? []).some((element) => tagFilters.includes(element)));
     }
     if (positionFilters.length > 0) {
-      result = result.filter((q) => q.position.some((p) => positionFilters.includes(p)));
+      result = result.filter((q) => (q.position ?? []).some((p) => positionFilters.includes(p)));
     }
     return result;
   }, [questions, tagFilters, positionFilters]);
